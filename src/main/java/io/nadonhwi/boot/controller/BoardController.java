1583349d23a0d21df081d77f5b1946d61ee845e3
donhwi94 <ehsgnl123@naver.com>
2022-05-05 21:11:24 +0900

    annotation을 이용한 validation 체크

--- src/main/java/io/nadonhwi/boot/controller/BoardController.java
+++ src/main/java/io/nadonhwi/boot/controller/BoardController.java
@@ -2,9 +2,12 @@ package io.nadonhwi.boot.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +46,10 @@ public class BoardController {
 	}
 	
 	@PostMapping("/form")
-	public String formSubmit(@ModelAttribute Board board) {
+	public String formSubmit(@Valid Board board, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "board/form";
+		}
 		boardRepository.save(board);
 		return "redirect:/board/list";
 	}
