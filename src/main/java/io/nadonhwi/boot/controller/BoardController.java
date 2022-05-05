c03e2cfd68d834b351fac1c872c9b1991a540d30
donhwi94 <ehsgnl123@naver.com>
2022-05-05 20:10:22 +0900

    form - th:object, th:field 를 사용해 등록 기능 구현

--- src/main/java/io/nadonhwi/boot/controller/BoardController.java
+++ src/main/java/io/nadonhwi/boot/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.nadonhwi.boot.model.Board;
@@ -24,5 +26,17 @@ public class BoardController {
 		model.addAttribute("boards", boards);
 		
 		return "board/list";
+	}
+	
+	@GetMapping("/form")
+	public String form(Model model) {
+		model.addAttribute("board", new Board());
+		return "board/form";
+	}
+	
+	@PostMapping("/form")
+	public String formSubmit(@ModelAttribute Board board) {
+		boardRepository.save(board);
+		return "redirect:/board/list";
 	}
 }
