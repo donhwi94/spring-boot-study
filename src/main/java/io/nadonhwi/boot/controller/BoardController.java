16b3eb71753617eddffe33f60c3d1cc4ae9fb8ed
donhwi94 <ehsgnl123@naver.com>
2022-05-07 23:57:29 +0900

    pageable을 이용한 페이징 구현

--- src/main/java/io/nadonhwi/boot/controller/BoardController.java
+++ src/main/java/io/nadonhwi/boot/controller/BoardController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -29,8 +33,23 @@ public class BoardController {
 	private BoardValidator boardValidator;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Board> boards = boardRepository.findAll();
+	public String list(Model model, @PageableDefault(size=2) Pageable pageable) {
+		
+		// page별 조회 
+		// localhost:8090/board/list?page=0&size=1 과 같은식으로 표시할 page와 size를 파라미터로 넘겨줄 수 있다
+		Page<Board> boards = boardRepository.findAll(pageable);
+		
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4); 
+		
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+		// page별 조회 : 하드코딩
+		//Page<Board> boards = boardRepository.findAll(PageRequest.of(0, 20));
+		
+		// 전체 리스트 조회
+		// List<Board> boards = boardRepository.findAll();
+		
 		model.addAttribute("boards", boards);
 		
 		return "board/list";
