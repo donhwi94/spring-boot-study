68d5cf264f2b44f9e45f99cbef5c32a082696090
donhwi94 <ehsgnl123@naver.com>
2022-05-14 22:38:25 +0900

    h2 database 데이터 자동생성

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,4 @@
+insert into role (name) values ('관리자');
+
+insert into board (content, title, user_id) values ('첫번쨰 게시물', '안녕하세요', 1);
+insert into board (content, title, user_id) values ('두번째', '반갑습니다', 1);
